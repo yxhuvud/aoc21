52e9d6d784ccfdc64831c67bf99927b3ee26a457
Linus Sellberg <sellberg@gmail.com>
2021-12-06 08:58:30 +0100

    Refactor day6 to deque.

--- day6.cr
+++ day6.cr
@@ -1,16 +1,14 @@
 inputs = File.read("input.day6").split(',').map(&.to_i8)
 
 def solve(n, inputs)
-  by_day = inputs.tally.transform_values { |v| v.to_i64 }
-  n.times do |i|
-    by_day = by_day.transform_keys { |k| k -= 1 }
-    by_day[6] ||= 0i64
-    by_day[8] = by_day[-1]? || 0i64
-    by_day[6] += by_day.delete(-1) || 0
+  deq = Deque(Int64).new(9) { 0i64 }
+  inputs.each { |v| deq[v] += 1 }
+  n.times do
+    deq.rotate! 1
+    deq[6] += deq[8]
   end
-  by_day.values.sum
+  deq.sum
 end
 
 puts "part1: #{solve(80, inputs)}"
 puts "part2: #{solve(256, inputs)}"
-
