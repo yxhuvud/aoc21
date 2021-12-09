e49f1e909e5fcb940b1cba555d7f14d6a6f2edc9
Linus Sellberg <sellberg@gmail.com>
2021-12-09 16:57:43 +0100

    Day9 cleanup

--- day9.cr
+++ day9.cr
@@ -1,30 +1,25 @@
 map = File.read("input.day9")
   .lines.map(&.chars.map(&.to_i8))
 
-def neighbours(p : Pos)
-  {
-    Pos.new(p.x, p.y + 1),
-    Pos.new(p.x, p.y - 1),
-    Pos.new(p.x + 1, p.y),
-    Pos.new(p.x - 1, p.y),
-  }
-end
+record(Pos, x : Int8, y : Int8) do
+  def neighbours
+    {
+      Pos.new(x, y + 1), Pos.new(x + 1, y),
+      Pos.new(x, y - 1), Pos.new(x - 1, y),
+    }
+  end
 
-def above_threshold?(pos, map, v)
-  pos.x < 0 || pos.x >= map.size || pos.y < 0 || pos.y >= map[0].size ||
-    map[pos.x][pos.y] > v
-end
-
-record(Pos, x : Int32, y : Int32)
-
-min_points = Array(Pos).new
-map.each_with_index do |row, x|
-  row.each_with_index do |v, y|
-    if neighbours(Pos.new(x, y)).all? { |n| above_threshold?(n, map, v) }
-      min_points << Pos.new(x, y)
-    end
+  def above_threshold?(map, v)
+    x < 0 || x >= map.size || y < 0 || y >= map[0].size ||
+      map[x][y] > v
   end
 end
+
+min_points = 0.to(map.size - 1).flat_map do |x|
+  0.to(map[0].size - 1)
+    .map { |y| Pos.new(x.to_i8, y.to_i8) }
+    .select { |p| p.neighbours.all? { |n| n.above_threshold?(map, map[p.x][p.y]) } }
+end.to_a
 
 puts "part1: #{min_points.sum(0) { |p| map[p.x][p.y] } + min_points.size}"
 
@@ -35,10 +30,10 @@ val = min_points.map do |p|
   seen << p
   queue << p
   while p = queue.pop?
-    neighbours(p).each do |pp|
-      next if above_threshold?(pp, map, 8) || seen.includes?(pp)
-      queue << pp unless seen.includes?(pp)
-      seen << pp
+    p.neighbours.each do |n|
+      next if n.above_threshold?(map, 8) || seen.includes?(n)
+      queue << n unless seen.includes?(n)
+      seen << n
     end
   end
   seen.size
