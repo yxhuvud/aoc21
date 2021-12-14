a76c4c256f6d91364ce8a75c3a056946f65881ed
Linus Sellberg <sellberg@gmail.com>
2021-12-14 07:09:07 +0100

    Day 14.

---
+++ day14.cr
@@ -0,0 +1,35 @@
+def step(pairs, rules)
+  counts = Hash(Tuple(Char, Char), Int64).new { 0i64 }
+  pairs.each do |pair, c|
+    rules[pair].each { |p| counts[p] += c }
+  end
+  counts
+end
+
+def solve(pairs, rules, n, first, last)
+  n.times { pairs = step(pairs, rules) }
+
+  counts = Hash(Char, Int64).new { 0i64 }
+  counts[first] = counts[last] = 1
+  pairs.each do |(c1, c2), v|
+    counts[c1] += v
+    counts[c2] += v
+  end
+
+  (counts.values.max - counts.values.min) // 2
+end
+
+inputs = File.read("input.day14").lines
+start = inputs.shift.chars
+pairs = start.each_cons(2).map { |cs| {cs[0], cs[1]} }.tally
+first, last = start[0], start[-1]
+
+inputs.shift
+
+rules = inputs.map(&.split(" -> ")).to_h do |vs|
+  cs = vs[0].chars
+  { {cs[0], cs[1]}, { {cs[0], vs[1][0]}, {vs[1][0], cs[1]} } }
+end
+
+p solve(pairs, rules, 10, first, last)
+p solve(pairs, rules, 40, first, last)
